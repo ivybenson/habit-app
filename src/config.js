30d466bd3c542cf76f408a9aaa6e9039965d27b7
Ivy Benson <irosebenson@gmail.com>
2020-12-27 17:18:04 -0600

    updates for deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  API_ENDPOINT: `http://localhost:8000`,
+  API_ENDPOINT: `https://damp-lake-87085.herokuapp.com/`,
   TOKEN_KEY: `habit-tracker-token`,
-  API_BASE_URL: "",
 };
